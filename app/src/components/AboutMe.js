786ca6331cd9facf7079b34df9fec84dfc627c50
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-12-17 17:20:56 -0500

    Add AboutMe component

---
+++ app/src/components/AboutMe.js
@@ -0,0 +1,12 @@
+const AboutMe = () => {
+  return (
+    <div>
+      <h4>About Me</h4>
+      <p style={{width: '70vw', maxWidth: '400px', margin: '0 auto'}}>
+        Hello! My name is Chris Kay and I'm a full stack software engineer from Atlanta, Georgia. As an intensely curious person, I've always been fascinated by technology. I'm interested in finding a position as a junior developer at a company where I can learn from more experienced engineers and improve and expand my skillset. 
+      </p>
+    </div>
+  )
+}
+
+export default AboutMe

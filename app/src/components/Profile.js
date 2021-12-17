9ac02303a9b42be8b43f61d166e9ea16a000b014
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-12-17 17:17:09 -0500

    Add a Profile component for personal info

---
+++ app/src/components/Profile.js
@@ -0,0 +1,13 @@
+const Profile = () => {
+  return (
+    <section className='profile-container'>
+      <div className='profile-header'>
+        <h2 style={{fontSize: '3rem', fontWeight: 300, color: 'grey'}}>Profile</h2>
+        <p>I'm a full stack web developer</p>
+        <div style={{width: '70vw', height: '2px', background: 'mediumseagreen', margin: '20px auto'}} />
+      </div>
+    </section>
+  )
+}
+
+export default Profile

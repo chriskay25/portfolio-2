fd23163d42c92763ce2cbcf034aa7a46c827c28c
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-12-18 01:29:48 -0500

    Add Profile comp to App

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import Intro from './components/Intro'
+import Profile from './components/Profile'
 
 function App() {
   return (
     <div className='app'>
       <Intro />
+      <Profile />
     </div>
   )
 }

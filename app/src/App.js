5255f8a553f764a61f380080c75975fde5046457
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-12-19 03:12:59 -0500

    Add Skills and Footer to App

--- app/src/App.js
+++ app/src/App.js
@@ -2,12 +2,16 @@ import React from 'react'
 import './App.css'
 import Intro from './components/Intro'
 import Profile from './components/Profile'
+import Skills from './components/Skills'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className='app'>
       <Intro />
       <Profile />
+      <Skills />
+      <Footer />
     </div>
   )
 }

ec1b6b5a91a2d9969d385b8fa6447e77e924f34d
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-12-19 03:08:50 -0500

    Import components that were extracted out before

--- app/src/components/Profile.js
+++ app/src/components/Profile.js
@@ -1,10 +1,19 @@
+import AboutMe from './AboutMe'
+import ProfileDetails from './ProfileDetails'
+
 const Profile = () => {
   return (
-    <section className='profile-container'>
-      <div className='profile-header'>
-        <h2 style={{fontSize: '3rem', fontWeight: 300, color: 'grey'}}>Profile</h2>
-        <p>I'm a full stack web developer</p>
-        <div style={{width: '70vw', height: '2px', background: 'mediumseagreen', margin: '20px auto'}} />
+    <section className='profile'>
+      <div className='profile-container'>
+        <div className='profile-header'>
+          <h2 style={{fontSize: '3rem', fontWeight: 300, color: 'grey'}}>Profile</h2>
+          <p style={{fontSize: '1.7rem'}}>Full Stack Web Developer</p>
+          <div style={{width: '60vw', height: '2px', background: 'mediumseagreen', margin: '20px auto'}} />
+        </div>
+
+        <AboutMe />
+        <div className='pic' />
+        <ProfileDetails />
       </div>
     </section>
   )

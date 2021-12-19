5c13cb93d006d9f6b784b79e84df3cc913b8b2de
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-12-19 03:06:34 -0500

    Add details

--- app/src/components/ProfileDetails.js
+++ app/src/components/ProfileDetails.js
@@ -1,7 +1,13 @@
 const ProfileDetails = () => {
   return (
     <div className='profile-details'>
-      <h4>Details</h4>
+      <h3 style={{fontWeight: 400, fontSize: '1.6rem', color: 'grey'}}>Details</h3>
+      <p style={{color: 'grey', fontWeight: 'bold'}}>Name:</p>
+      <p>Chris Kay</p>
+      <p style={{color: 'grey', fontWeight: 'bold'}}>Age:</p>
+      <p>37</p>
+      <p style={{color: 'grey', fontWeight: 'bold'}}>Location:</p>
+      <p>Atlanta, GA</p>
     </div>
   )
 }

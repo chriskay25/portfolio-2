958504063da871d2e51adffee2446789d4793ef6
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-12-19 02:57:30 -0500

    Add a Skills component

---
+++ app/src/components/Skills.js
@@ -0,0 +1,12 @@
+const Skills = () => {
+  return (
+    <section className='skills'>
+      <div className='skills-header'>
+          <h2 style={{fontSize: '3rem', fontWeight: 300, color: 'grey'}}>Skills</h2>
+          <div style={{width: '60vw', height: '2px', background: 'mediumseagreen', margin: '20px auto'}} />
+        </div>
+    </section>
+  )
+}
+
+export default Skills

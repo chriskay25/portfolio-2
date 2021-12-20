83d8c52eb28c0782757f729687b8792f1b88824e
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-12-20 00:17:44 -0500

    Import Stack and Proficiencies components

--- app/src/components/Skills.js
+++ app/src/components/Skills.js
@@ -1,10 +1,16 @@
+import Stack from './Stack'
+import Proficiencies from './Proficiencies'
+
 const Skills = () => {
   return (
     <section className='skills'>
       <div className='skills-header'>
-          <h2 style={{fontSize: '3rem', fontWeight: 300, color: 'grey'}}>Skills</h2>
-          <div style={{width: '60vw', height: '2px', background: 'mediumseagreen', margin: '20px auto'}} />
-        </div>
+        <h2 style={{fontSize: '3rem', fontWeight: 300, color: 'grey'}}>Skills</h2>
+        <div style={{width: '60vw', height: '2px', background: 'mediumseagreen', margin: '20px auto'}} />
+      </div>
+      
+      <Stack />
+      <Proficiencies />
     </section>
   )
 }

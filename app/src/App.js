8c09e3191706bad605adb679520b466fa047342d
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-12-30 13:55:53 -0500

    Add a fn that collects refs from components to pass to Nav for quick scrolling

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import Nav from './components/Nav'
 import Intro from './components/Intro'
@@ -6,16 +6,38 @@ import Profile from './components/Profile'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Footer from './components/Footer'
+import ScrollToTop from './components/ScrollToTop'
 
-function App() {
+const App = () => {
+  const [profileRef, setProfileRef] = useState(null)
+  const [skillsRef, setSkillsRef] = useState(null)
+  const [projectsRef, setProjectsRef] = useState(null)
+  const [contactRef, setContactRef] = useState(null)
+
+  const collectRef = (sectionRef) => {
+    switch (sectionRef.current.className) {
+      case 'profile':
+        return setProfileRef(sectionRef)
+      case 'skills':
+        return setSkillsRef(sectionRef)
+      case 'projects':
+        return setProjectsRef(sectionRef)
+      case 'footer':
+        return setContactRef(sectionRef)
+      default:
+        return
+    }
+  }
+
   return (
     <div className='app'>
-      <Nav />
+      <Nav profileRef={profileRef} skillsRef={skillsRef} projectsRef={projectsRef} contactRef={contactRef} />
       <Intro />
-      <Profile />
-      <Skills />
-      <Projects />
-      <Footer />
+      <Profile collectRef={collectRef} />
+      <Skills collectRef={collectRef} />
+      <Projects collectRef={collectRef} />
+      <Footer collectRef={collectRef} />
+      <ScrollToTop />
     </div>
   )
 }

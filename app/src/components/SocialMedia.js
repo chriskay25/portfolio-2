ce86b2ada18b03cf44ba085f006741111baa833d
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-01-04 16:46:38 -0500

    Add links and put in unordered list, add new logos

--- app/src/components/SocialMedia.js
+++ app/src/components/SocialMedia.js
@@ -1,19 +1,23 @@
-import liLogo from '../assets/linkedin-32.png'
-import ghLogo from '../assets/gh-light-sm.png'
+import liLogoDark from '../assets/li-dark.png'
+import ghLogoDark from '../assets/gh-dark.png'
 import { motion } from 'framer-motion'
 
 const SocialMedia = () => {
 
-  const handleLiClick = () => {
-    window.location.href = 'https://linkedin.com/in/chris-kay-4060bb61'
-  }
-  const handleGhClick = () => {
-    window.location.href = 'https://github.com/chriskay25'
-  }
   return (
-    <motion.div className='social-media' initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: 2.6, duration: .8, ease: 'easeIn'}}>
-      <img src={liLogo} alt='linked-in-logo' onClick={() => handleLiClick} />
-      <img src={ghLogo} alt='github-logo' onClick={() => handleGhClick()} />
+    <motion.div className='social-media' initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: .6, duration: .8, ease: 'easeIn'}}>
+      <ul style={{display: 'flex'}}>
+        <li className='social-media-icon'>
+          <a href='https://linkedin.com/in/chris-kay-4060bb61'>
+            <img src={liLogoDark} alt='linked-in-logo' />
+          </a>
+        </li>
+        <li className='social-media-icon'>
+          <a href='https://github.com/chriskay25'>
+            <img src={ghLogoDark} alt='github-logo' />
+          </a>
+        </li>
+      </ul>
     </motion.div>
   )
 }

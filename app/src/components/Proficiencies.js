0eac5b16062b9eb419b19ba1ce19e99b5b60ad9f
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-01-04 16:13:00 -0500

    Move css to stylesheet

--- app/src/components/Proficiencies.js
+++ app/src/components/Proficiencies.js
@@ -2,11 +2,11 @@ import { proficiencyData } from '../assets/data.js'
 
 const Proficiencies = () => {
   return (
-    <div className='proficiencies' style={{marginTop: '4rem'}}>
+    <div className='proficiencies'>
       <h3 style={{fontWeight: 400, fontSize: '1.8rem', color: 'grey'}}>PROFICIENCIES</h3>
-      <ul style={{display: 'flex', flexWrap: 'wrap', width: '70%', margin: '0 auto', justifyContent: 'center'}}>
+      <ul>
         {proficiencyData.map((p, i) => (
-          <li key={i} style={{width: 'fit-content', padding: '.5rem', margin: '1rem', border: 'black 3px solid'}}>
+          <li key={i}>
             <p>{p.proficiency}</p>
           </li>
         ))}

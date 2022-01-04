cd1a375a0d9e7006fdacb754626cec3f1f5b3081
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-01-04 17:41:06 -0500

    Expand on personal info, add picture

--- app/src/components/AboutMe.js
+++ app/src/components/AboutMe.js
@@ -1,10 +1,17 @@
 const AboutMe = () => {
   return (
-    <div>
-      <h4>About Me</h4>
-      <p style={{width: '70vw', maxWidth: '400px', margin: '0 auto'}}>
-        Hello! My name is Chris Kay and I'm a full stack software engineer from Atlanta, Georgia. As an intensely curious person, I've always been fascinated by technology. I'm interested in finding a position as a junior developer at a company where I can learn from more experienced engineers and improve and expand my skillset. 
+    <div className='about-me'>
+      <h3 className='section-subtitle'>About Me</h3>
+      <p>
+        I'm a full stack web developer from Atlanta, Georgia. Professionally, I've worked in sales and account management and gained valuable communication skills doing so, but I'm very excited to finally step into an analytical, technical role. I recently graduated from Flatiron School's software engineering program, where I learned full stack web development and computer science fundamentals.
       </p>
+
+      <div className='pic' />
+      
+      <p>
+        As a new developer, I'd like to find a position at a company where I can learn from more experienced engineers and improve and expand my skillset. Having lived in Atlanta for many years, it would be wonderful to find a position with an Atlanta organization. That being said, I'm open to remote work with any US companies that could use my skills.
+      </p>
+
     </div>
   )
 }

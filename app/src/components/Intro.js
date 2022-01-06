eb2b4f312bfda4ce4a83f4610745de20696835f6
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-01-05 20:11:33 -0500

    To fix error messages, make all colors uniform style

--- app/src/components/Intro.js
+++ app/src/components/Intro.js
@@ -11,7 +11,7 @@ const Intro = () => {
       />
       <img className='intro-background-skyline' src={skyline} alt='atlanta-skyline' />
       <motion.h1 initial={{x: '100vw', y: '-0px'}} animate={{x: 0, y: 0}} transition={{duration: 1.5}} style={{fontSize: '4.2rem'}}>CHRIS KAY</motion.h1>
-      <motion.div initial={{height: '.2rem', width: '0rem'}} animate={{height: '.2rem', width: '20rem', background: 'white'}} transition={{delay: 1, duration: .7, ease: 'linear'}}></motion.div>
+      <motion.div initial={{height: '.2rem', width: '0rem'}} animate={{height: '.2rem', width: '20rem', background: '#fff'}} transition={{delay: 1, duration: .7, ease: 'linear'}}></motion.div>
       <motion.h4 initial={{x: '-100vw', y: '0px'}} animate={{x: 0, y: 0}} transition={{duration: 1.5}} style={{fontSize: '1.5rem', fontWeight: '600'}}>FULL STACK WEB DEVELOPER</motion.h4>
       <motion.h5 initial={{opacity: 0, x: '0', y: '5vh'}} animate={{opacity: 1, x: 0, y: 0}} transition={{delay: 1, duration: .8}} style={{fontSize: '1.2rem', fontWeight: '600'}}>ATLANTA, GA</motion.h5>
     </section>

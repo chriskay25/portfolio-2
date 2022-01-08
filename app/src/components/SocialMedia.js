0d4a2df97eda1844ec50556995dafdb1294e60d0
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-01-07 22:08:06 -0500

    Add a class for social media links, edit footer styles

--- app/src/components/SocialMedia.js
+++ app/src/components/SocialMedia.js
@@ -5,21 +5,29 @@ import { motion } from 'framer-motion'
 const SocialMedia = () => {
 
   return (
-    <motion.div className='social-media' initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: .6, duration: .8, ease: 'easeIn'}}>
-      <ul style={{display: 'flex'}}>
-        <li className='social-media-icon'>
-          <a href='https://linkedin.com/in/chris-kay-4060bb61'>
-            <img src={liLogoDark} alt='linked-in-logo' />
+    <motion.div
+      className="social-media"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 0.6, duration: 0.8, ease: "easeIn" }}
+    >
+      <ul style={{ display: "flex" }}>
+        <li className="social-media-icon">
+          <a
+            className="social-media-link"
+            href="https://linkedin.com/in/chris-kay-4060bb61"
+          >
+            <img src={liLogoDark} alt="linked-in-logo" />
           </a>
         </li>
-        <li className='social-media-icon'>
-          <a href='https://github.com/chriskay25'>
-            <img src={ghLogoDark} alt='github-logo' />
+        <li className="social-media-icon">
+          <a className="social-media-link" href="https://github.com/chriskay25">
+            <img src={ghLogoDark} alt="github-logo" />
           </a>
         </li>
       </ul>
     </motion.div>
-  )
+  );
 }
 
 export default SocialMedia

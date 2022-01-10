6d4ec207a4a99264d80f2070223f9031e13bc971
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-01-09 23:59:40 -0500

    Add motion to manage layout changes, add class to div

--- app/src/components/Projects.js
+++ app/src/components/Projects.js
@@ -1,6 +1,7 @@
 import { useRef, useEffect } from 'react'
 import Project from './Project'
 import { projectData } from '../assets/data'
+import { motion } from 'framer-motion'
 
 const Projects = ({ collectRef }) => {
   const ref = useRef(null)
@@ -16,12 +17,12 @@ const Projects = ({ collectRef }) => {
           <h2 className='section-title'>PROJECTS</h2>
           <div className='section-title-underline' />
         </div>
-        <div style={{}}>
-          <ul>
+        <div className='projects-container'>
+          <motion.ul style={{}} layout>
             {projectData.map((p, i) => (
               <Project key={i} project={p} />
             ))}
-          </ul>
+          </motion.ul>
         </div>
       </div>
     </section>

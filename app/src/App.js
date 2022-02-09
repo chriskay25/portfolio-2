3aba4808b36932c9d6d77fcac60d48b9001481ad
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2022-02-09 15:44:36 -0500

    Prettier edits

--- app/src/App.js
+++ app/src/App.js
@@ -1,37 +1,42 @@
-import React, { useState } from 'react'
-import './App.css'
-import Nav from './components/Nav'
-import Intro from './components/Intro'
-import Profile from './components/Profile'
-import Skills from './components/Skills'
-import Projects from './components/Projects'
-import Footer from './components/Footer'
-import ScrollToTop from './components/ScrollToTop'
+import React, { useState } from "react";
+import "./App.css";
+import Nav from "./components/Nav";
+import Intro from "./components/Intro";
+import Profile from "./components/Profile";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 const App = () => {
-  const [profileRef, setProfileRef] = useState(null)
-  const [skillsRef, setSkillsRef] = useState(null)
-  const [projectsRef, setProjectsRef] = useState(null)
-  const [contactRef, setContactRef] = useState(null)
+  const [profileRef, setProfileRef] = useState(null);
+  const [skillsRef, setSkillsRef] = useState(null);
+  const [projectsRef, setProjectsRef] = useState(null);
+  const [contactRef, setContactRef] = useState(null);
 
   const collectRef = (sectionRef) => {
     switch (sectionRef.current.className) {
-      case 'profile':
-        return setProfileRef(sectionRef)
-      case 'skills':
-        return setSkillsRef(sectionRef)
-      case 'projects':
-        return setProjectsRef(sectionRef)
-      case 'footer':
-        return setContactRef(sectionRef)
+      case "profile":
+        return setProfileRef(sectionRef);
+      case "skills":
+        return setSkillsRef(sectionRef);
+      case "projects":
+        return setProjectsRef(sectionRef);
+      case "footer":
+        return setContactRef(sectionRef);
       default:
-        return
+        return;
     }
-  }
+  };
 
   return (
-    <div className='app'>
-      <Nav profileRef={profileRef} skillsRef={skillsRef} projectsRef={projectsRef} contactRef={contactRef} />
+    <div className="portfolio-app">
+      <Nav
+        profileRef={profileRef}
+        skillsRef={skillsRef}
+        projectsRef={projectsRef}
+        contactRef={contactRef}
+      />
       <Intro />
       <Profile collectRef={collectRef} />
       <Skills collectRef={collectRef} />
@@ -39,7 +44,7 @@ const App = () => {
       <Footer collectRef={collectRef} />
       <ScrollToTop />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
